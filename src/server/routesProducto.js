557c27c5c6016e0beedee20aa4fbe0b6a0683b0d
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-10-21 11:31:32 -0500

    New routes

--- src/server/routesProducto.js
+++ src/server/routesProducto.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { db } = require("./db.js");
 const routes = express.Router();
 
+const { MongoClient } = require("mongodb");
+
+
 routes.get("/", (req, res) => {
   db("SELECT * FROM [dbo].[producto];", (record) => {
     const r = record.recordsets;
@@ -9,14 +12,42 @@ routes.get("/", (req, res) => {
   });
 });
 
-//SELECT según ID Producto
-routes.get("/:id", (req, res) => {
-  let id = req.params.id;
-  db(`SELECT * FROM [dbo].[producto] WHERE idProducto = ${id};`, (record) => {
-    const r = record.recordsets;
-    res.send(r);
-  });
+//SELECT Productos según ID TIENDA
+routes.get("/:idTienda", (req, res) => {
+  const idTienda = req.params.idTienda;
+  async function seleccion() {
+    const uri =
+      "mongodb+srv://bratty289:YGTl63QI@pruebamongo.lnhsrdp.mongodb.net/test";
+    const client = new MongoClient(uri);
+
+    try {
+      await client.connect();
+      const resultado = await findProducts(client, idTienda);
+      res.send(resultado);
+    } finally {
+      await client.close();
+    }
+  }
+  seleccion().catch(console.error);
 });
+
+async function findProducts(client, tiendaID) {
+  const result = await client
+    .db(`tienda-${tiendaID}`)
+    .collection("productos")
+    .find({}).toArray();
+
+  if (result) {
+    console.log(
+      `Se encontró productos en la tienda ${tiendaID}':`
+    );
+    console.log(result);
+    return result;
+  } else {
+    console.log(`No se encontró productos en la tienda '${tiendaID}'`);
+  }
+}
+
 
 //SELECT según ID Categoria
 routes.get("/categoria/:id", (req, res) => {
