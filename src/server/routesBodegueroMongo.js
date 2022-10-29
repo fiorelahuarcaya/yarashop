cef6c3bbd406f87426aa5d10f9608db6a77b68f1
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-10-28 21:49:08 -0500

    todoMongo

--- src/server/routesBodegueroMongo.js
+++ src/server/routesBodegueroMongo.js
@@ -6,6 +6,17 @@ const uri =
   "mongodb+srv://bratty289:YGTl63QI@pruebamongo.lnhsrdp.mongodb.net/test";
 const client = new MongoClient(uri);
 
+routes.get("/:id", async (req, res) => {
+  const id = req.params.id * 1.0;
+  try {
+    await client.connect();
+    const resultado = await findUserbyId(client, id);
+    res.send(resultado);
+  } finally {
+    await client.close();
+  }
+});
+
 routes.get("/", async (req, res) => {
   try {
     await client.connect();
@@ -35,15 +46,47 @@ routes.post("/", async (req, res) => {
   }
 });
 
-routes.delete("/:id", (req, res) => {});
+routes.delete("/:id", async (req, res) => {
+  const idAdmin = req.params.id * 1.0;
+  try {
+    await client.connect();
+    const result = await deletebyId(client, idAdmin);
+    res.send(result);
+  } finally {
+    await client.close();
+  }
+});
 
-routes.put("/:id", (req, res) => {});
+routes.put("/:id", async (req, res) => {
+  let idAdmin = req.params.id * 1.0;
+  const usuario = {
+    nombreCompleto: req.body.nombreCompleto,
+    correo: req.body.correo,
+  };
+
+  try {
+    await client.connect();
+    const result = await updatebyId(client, idAdmin, usuario);
+    res.send(result);
+  } finally {
+    await client.close();
+  }
+});
 
 async function createUser(client, newUser) {
   const result = await client
     .db("yarashop")
     .collection("bodeguero")
     .insertMany(newUser);
+  return result;
+}
+
+async function findUserbyId(client, idAdmin) {
+  const result = await client
+    .db("yarashop")
+    .collection("bodeguero")
+    .findOne({ idAdmin: idAdmin });
+
   return result;
 }
 
@@ -58,6 +101,26 @@ async function findProducts(client) {
   } else {
     console.log(`No se encontró productos en yarashop`);
   }
+}
+
+async function deletebyId(client, idAdmin) {
+  const result = await client
+    .db("yarashop")
+    .collection("bodeguero")
+    .deleteOne({ idAdmin: idAdmin });
+  console.log(`${result.deletedCount} document(s) was/were deleted.`);
+  return result;
+}
+
+async function updatebyId(client, idAdmin, modifiedUser) {
+  const result = await client
+    .db("yarashop")
+    .collection("bodeguero")
+    .updateOne({ idAdmin: idAdmin }, { $set: modifiedUser });
+
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+  return result;
 }
 
 module.exports = routes;
