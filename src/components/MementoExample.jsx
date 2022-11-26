e34eff7e55ca5558ab42ae16fceb6a0450be074f
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-11-25 22:24:28 -0500

    bridge

---
+++ src/components/MementoExample.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+// Contador: Clic -> Aumenta en uno ; restore -> memento
+let Counter = (props, ref) => {
+  const [count, setCount] = React.useState(0);
+
+  React.useImperativeHandle(ref, () => ({
+    createMemento: () => {
+      console.log("Created memento with count " + count);
+      return { count };
+    },
+    restore: (memento) => {
+      console.log("Restored memento");
+      setCount(memento.count);
+    },
+  }));
+
+  return (
+    <div
+      children={count}
+      onClick={() => setCount((prevCount) => prevCount + 1)}
+      style={{ fontSize: "32px", userSelect: "none" }}
+    />
+  );
+};
+
+Counter = React.forwardRef(Counter);
+
+// Objeto cliente que crea el contador y los mementos.
+const Client = () => {
+  const ref = new React.createRef();
+
+  const [memento, setMemento] = React.useState();
+
+  return (
+    <React.Fragment>
+      <Counter ref={ref} />
+
+      <button
+        onClick={() => setMemento(ref.current.createMemento())}
+        children="Create memento"
+      />
+      <button
+        onClick={() => ref.current.restore(memento)}
+        children="Restore memento"
+      />
+    </React.Fragment>
+  );
+};
+
+export default Client;
